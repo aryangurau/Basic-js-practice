357f81f96218acdee87bf90243f4c650e6047ec0
aryangurau <aryangurau143@gmail.com>
2024-07-27 11:38:33 +0545

    learned about js basics

---
+++ JavaScript/variables.js
@@ -0,0 +1,12 @@
+//Es5
+var person = "aryan gurau"; //it can be redeclared multiple times
+
+//ES6
+const anotherPerson = "abhishake"; //it cannot be redeclared and reused again
+let thirdPerson = "sita"; // can be redeclared and lets update the value
+
+console.log(person, anotherPerson, thirdPerson);
+
+var test = "raktim";
+var test = "sita";
+console.log(test); //it overwrites first test
