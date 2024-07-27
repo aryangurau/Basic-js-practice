1507c6e7859ef9c22464bf4bf1f0dbf7bda02a78
aryangurau <aryangurau143@gmail.com>
2024-07-27 16:06:28 +0545

    learned es5 and es6 functions and completed activity

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -26,14 +26,50 @@ console.log({ result , result1});
 
 */
 
-/*
 //write a js program to find the square of  number
+/*  
+-------------ES5----------------
+*/
+
+/*
 function square(n) {
   const a = n * n;
   return a;
 }
-const result = square(8);
+const num = Number(prompt("enter a number"));
+if (isNaN(num)) {
+  alert("you didnot entered number");
+}
+const result = square(num);
 console.log(result);
-*/
 
 // -------------ES6---------------
+const squares = (x) => {
+  return x * x;
+};
+const newAnswer = squares(4);
+console.log({ newAnswer });
+
+
+//write a js program to convert celsius into fareheight
+
+//---ES5-------
+function convertCelsius(c) {
+  const f = c * (9 / 5) + 32;
+  return f;
+}
+const ask = Number(prompt("enter temperature in celsius"));
+const fareheight = convertCelsius(ask);
+console.log({ fareheight });
+
+//ES6
+const temperature = (c) => {
+  far = c * (9 / 5) + 32;
+  return far;
+};
+const provideValue = Number(prompt("enter temp in celsius"));
+const newFareheight = temperature(provideValue);
+console.log(newFareheight);
+
+
+*/
