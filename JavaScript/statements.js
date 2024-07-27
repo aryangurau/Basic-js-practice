37d0eb8802638727d7fce92c20aef7125af90c9c
aryangurau <aryangurau143@gmail.com>
2024-07-27 13:47:47 +0545

    learned about statements

---
+++ JavaScript/statements.js
@@ -0,0 +1,100 @@
+// statements
+// 1. if else
+// 2. switch case
+
+//if else syntax
+
+//if (condition)
+//{
+// if logic
+// }
+// else{
+//     else logic
+// }
+
+/*const gender = confirm("are you male?");
+if (gender) {
+  alert("you are male"); //if true
+} else {
+  alert("you are female"); //if false
+}
+ */
+
+// prompt to ask user to reply m or f
+
+/*
+const gender = prompt("enter your gender");
+if (gender === "m") {
+  alert("you are male");
+} else {
+  alert("you are female");
+}
+*/
+//  = variable assign
+//  == data type and value compare
+// === data type and value compare + data conversion to boolean
+
+//Switch statement//.....................
+//write a js program to check the day of the week
+/*
+const day = Number(prompt("enter the 1-7 to check the day of week"));
+switch (day) {
+  case 1:
+    alert("its sunday");
+    break;
+  case 2:
+    alert("its monday");
+    break;
+  case 3:
+    alert("its tuesday");
+    break;
+  case 4:
+    alert("its wednesday");
+    break;
+  case 5:
+    alert("its thursday");
+    break;
+  case 6:
+    alert("its friday");
+    break;
+  case 7:
+    alert("its saturday");
+    break;
+  default:
+    alert("enter the correct choice of number");
+    break;
+}
+*/
+/*
+//write a js program to check the grade of the student using switch case
+//A:  >=80
+//B:  >=70 but less than 80
+
+const score = Number(prompt("enter the marks"));
+switch (true) {
+  case score >= 80:
+    alert("your grade is A");
+    break;
+  case score >= 70 && score < 80:
+    alert("your grade is B");
+    break;
+
+  default:
+    alert("you are failed :better luck next time");
+}
+*/
+
+//Es5
+// const gender ="m"
+// if (gender="m")
+// {
+//     console.log("m")
+// }
+// else{
+//     console.log("f")
+// }
+
+//Es6
+syntax;
+// const gender = "m";
+//gender==="m" ?console.log('m'):console.log('f'); //m
