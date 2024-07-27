1b19048f136d12748d9927d6fae7ee9b1fa0fbf9
aryangurau <aryangurau143@gmail.com>
2024-07-27 12:09:30 +0545

    learned about user interaction and made sum calculator

---
+++ JavaScript/interaction.js
@@ -0,0 +1,24 @@
+//user interactions
+
+/*
+//alert
+alert("Hello aryan");
+
+//prompt
+
+const age = prompt("How old are you?");
+
+//confirm
+const confirmAge = confirm("Are you above 18?");
+console.log(age, confirmAge);
+
+alert(`you are ${age} years old`); //string literal 
+*/
+//write a javascript program to create a sum calculator user interaction
+
+alert("This performs sum calculations");
+
+const value1 = parseInt(prompt("enter first number"));
+const value2 = parseInt(prompt("enter second number"));
+
+alert(`The sum of two number is ${value1 + value2}`);
