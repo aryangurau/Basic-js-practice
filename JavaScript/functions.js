29b094ac692f8939b2f881205165821cece97f02
aryangurau <aryangurau143@gmail.com>
2024-07-27 15:41:16 +0545

    created functions

---
+++ JavaScript/functions.js
@@ -0,0 +1,39 @@
+/*
+function are the main building block of any program
+2 core principl
+  a. DRY ( Donot repeat yourself)
+  b. SoC(seperation of Concern)
+   
+*/
+
+/*
+// 2 ways to write function  ES5 and ES6
+
+// -------ES5-----------
+//  Write a sum function for adding 2numbers
+
+//function declare, name of function , parameters a and b
+function sum(a, b) {
+  //logic
+  const c = a + b;
+  return c;
+}
+
+// function call or execute
+const result = sum(2, 3);
+const result1 = sum(4, 4);
+console.log({ result , result1});
+
+*/
+
+/*
+//write a js program to find the square of  number
+function square(n) {
+  const a = n * n;
+  return a;
+}
+const result = square(8);
+console.log(result);
+*/
+
+// -------------ES6---------------
