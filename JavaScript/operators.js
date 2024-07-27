baac0b90cc9d49d9288147a93b0276ff0a1a9746
aryangurau <aryangurau143@gmail.com>
2024-07-27 14:06:57 +0545

    learned operators

---
+++ JavaScript/operators.js
@@ -0,0 +1,26 @@
+//  Three operators
+//  1. AND  ---&&
+//  2. OR   ---||
+//  3. NOT  ---!
+
+// 1. AND (both condition must be true)
+//  // condition1 && condition2
+//  //true true
+
+//if (condition1 && condition2)
+// {
+//     logic1
+//     else{
+//         else logic
+//     }
+// }
+// 2. OR (at least one condition must be true)
+//  // condition1 || condition2
+
+// 3. NOT ( opposite value)
+// const isAdult = false;
+// if (!isAdult) {
+//   console.log("you should watch cartoon");
+// } else {
+//   console.log("you are watching news");
+// }
