24b707d8ed3ff64545c5aabcd9b27f3449c3a590
aryangurau <aryangurau143@gmail.com>
2024-07-27 15:05:01 +0545

    learned loops in js

---
+++ JavaScript/loops.js
@@ -0,0 +1,59 @@
+//for syntax
+//for( begin; step condition; increment of step)
+// {
+//code to be executed
+// }
+
+/*  for loop execution-----------------
+//Write a js program for multiplication of table(prompt user to enter number)
+
+const table = Number(prompt("enter the number for multiplication table"));
+//if (typeof table != Number)
+if (isNaN(table)) {
+  alert("you didn't entered a number");
+}
+for (let i = 1; i <= 10; i++) {
+  console.log(`${table} x ${i} = ${table * i}`);
+}
+  */
+
+/*do while syntax...........
+begin step i=1
+do
+{//code execution}
+while(step condition i++)
+*/
+
+/*
+const table = Number(prompt("enter the number for multiplication"));
+if (isNaN(table)) {
+  alert("you didnt entered a number");
+}
+i = 1;
+do {
+  console.log(`${table} x ${i}= ${table * i}`);
+  i++;
+} while (i<= 10);
+*/
+
+/*while loop--------------------  
+while(step condition)
+{
+code execution
+i++(increment)
+
+}
+
+*/
+
+/*
+const table = Number(prompt("enter the number for multiplication"));
+if (isNaN(table)) {
+  alert("you didnt entered a number");
+}
+i = 1;
+while (i <= 10) {
+  console.log(`${table} x ${i}= ${table * i}`);
+  i++;
+}
+*/
